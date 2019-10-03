5073e63afef6394e1476f2c06c851a5f0501e76d
Natalie Estrada <nmestrad@gmail.com>
2019-10-03 09:24:59 -0500

    added button

---
+++ constants/ApiKeys.js
@@ -0,0 +1,9 @@
+import * as firebase from 'firebase';
+// Initialize Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyDRE85AhiqgPdSW_aY7OhGf3Ev3F9uTTq4",
+    authDomain: "wejay-254716.firebaseapp.com",
+    databaseURL: "https://wejay-254716.firebaseio.com",
+    storageBucket: "wejay-254716.appspot.com",
+  };
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
