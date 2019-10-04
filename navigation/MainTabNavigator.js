5465621f3b970f90cc40124ac9218641a5a1e2c6
Natalie Estrada <nmestrad@gmail.com>
2019-10-04 15:06:05 -0500

    stuff

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -5,10 +5,10 @@
 //   createBottomTabNavigator
 // } from 'react-navigation';
 
-import TabBarIcon from '../components/TabBarIcon';
-import HomeScreen from '../screens/HomeScreen';
-import LinksScreen from '../screens/LinksScreen';
-import SettingsScreen from '../screens/SettingsScreen';
+// import TabBarIcon from '../components/TabBarIcon';
+// import HomeScreen from '../screens/HomeScreen';
+// import LinksScreen from '../screens/LinksScreen';
+// import SettingsScreen from '../screens/SettingsScreen';
 //import JoinRoom from '../screens/JoinRoom'
 
 
