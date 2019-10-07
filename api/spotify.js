cf65a15f4fe4a5490d7134d2a834db756ad06e14
Christine Vargas <christineandreavargas@gmail.com>
2019-10-07 07:56:56 -0500

    updates

--- api/spotify.js
+++ api/spotify.js
@@ -1,147 +1,135 @@
-import {AuthSession} from 'expo'
-import { encode as btoa } from 'base-64'
-import { AsyncStorage } from 'react-native'
+import { AuthSession } from 'expo';
+import { encode as btoa } from 'base-64';
+import { AsyncStorage } from 'react-native';
 
 //need to writ/read accessToken, refreshToken, expirationtime to firestore
 
-
 //need all spotify requests in this file
 
-async function logIn(){
+async function logIn() {
+  try {
+    const redirect = 'https://auth.expo.io/@christinevargas/weJay';
+    const encodedRedirect = encodeURIComponent(redirect);
+    const ClientID = 'b7b6a836a01044abb7aa4eeb10c9039a';
+    const scopesArr = [
+      'playlist-modify-public',
+      'user-modify-playback-state',
+      'user-read-private',
+      'user-read-email'
+    ];
+    const scopes = encodeURIComponent(scopesArr.join(' '));
 
-    try {
-        const redirect = 'https://auth.expo.io/@gbuchanan/weJay'
-        const encodedRedirect = encodeURIComponent(redirect)
-        const ClientID = 'b7b6a836a01044abb7aa4eeb10c9039a'
-        const scopesArr = ['playlist-modify-public', 'user-modify-playback-state', 'user-read-private', 'user-read-email']
-        const scopes = encodeURIComponent(scopesArr.join(' '))
+    const result = await AuthSession.startAsync({
+      authUrl:
+        'https://accounts.spotify.com/authorize' +
+        '?client_id=' +
+        ClientID +
+        '&response_type=code' +
+        '&redirect_uri=' +
+        encodedRedirect +
+        (scopes ? '&scope=' + scopes : '')
+    });
 
-        const result = await AuthSession.startAsync({
-            authUrl:
-            'https://accounts.spotify.com/authorize' +
-            '?client_id=' +
-            ClientID +
-            '&response_type=code' +
-            '&redirect_uri=' +
-            encodedRedirect +
-            (scopes ? '&scope=' + scopes : '')
-        })
-
-        return result.params.code
-
-    }
-    catch (err){
-        console.log(err)
-    }
+    return result.params.code;
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-export async function getTokens(){
+export async function getTokens() {
+  try {
+    const authorizationCode = await logIn();
+    const ClientID = 'b7b6a836a01044abb7aa4eeb10c9039a';
+    const ClientSecret = process.env.SPOTIFY;
+    const redirect = 'https://auth.expo.io/@gbuchanan/weJay';
+    const encodedRedirect = encodeURIComponent(redirect);
+    const credsB64 = btoa(`${ClientID}:${ClientSecret}`);
 
-    try {
-        const authorizationCode = await logIn()
-        const ClientID = 'b7b6a836a01044abb7aa4eeb10c9039a'
-        const ClientSecret = process.env.SPOTIFY
-        const redirect = 'https://auth.expo.io/@gbuchanan/weJay'
-        const encodedRedirect = encodeURIComponent(redirect)
-        const credsB64 = btoa(`${ClientID}:${ClientSecret}`)
+    const response = await fetch('https://accounts.spotify.com/api/token', {
+      method: 'POST',
+      headers: {
+        Authorization: `Basic ${credsB64}`,
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: `grant_type=authorization_code&code=${authorizationCode}&redirect_uri=${encodedRedirect}`
+    });
 
-        const response = await fetch('https://accounts.spotify.com/api/token', {
-                method: 'POST',
-                headers: {
-                    Authorization: `Basic ${credsB64}`,
-                    'Content-Type': 'application/x-www-form-urlencoded',
-                },
-                body: `grant_type=authorization_code&code=${authorizationCode}&redirect_uri=${encodedRedirect}`,
-            })
+    const responseJSON = await response.json();
 
-        const responseJSON = await response.json()
+    const {
+      access_token: accessToken,
+      refresh_token: refreshToken,
+      expires_in: expiresIn
+    } = responseJSON;
 
-        const {
-            access_token: accessToken,
-            refresh_token: refreshToken,
-            expires_in: expiresIn
-        } = responseJSON
+    const expirationTime = new Date().getTime() + expiresIn * 1000;
 
-        const expirationTime = new Date().getTime() + expiresIn * 1000;
-
-        await setUserData('accessToken', accessToken)
-        await setUserData('refreshToken', refreshToken)
-        await setUserData('expirationTime', expirationTime.toString())
-    }
-    catch (e){
-        console.log(e)
-    }
+    await setUserData('accessToken', accessToken);
+    await setUserData('refreshToken', refreshToken);
+    await setUserData('expirationTime', expirationTime.toString());
+  } catch (e) {
+    console.log(e);
+  }
 }
 
+async function refreshTokens() {
+  try {
+    const ClientID = 'b7b6a836a01044abb7aa4eeb10c9039a';
+    const ClientSecret = process.env.SPOTIFY;
+    const credsB64 = btoa(`${ClientID}:${ClientSecret}`);
+    const refreshToken = await getUserData('refreshToken');
 
-async function refreshTokens(){
+    const response = await fetch('https://accounts.spotify.com/api/token', {
+      method: 'POST',
+      headers: {
+        Authorization: `Basic ${credsB64}`,
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: `grant_type=refresh_token&refresh_token=${refreshToken}`
+    });
 
-    try {
-        const ClientID = 'b7b6a836a01044abb7aa4eeb10c9039a'
-        const ClientSecret = process.env.SPOTIFY
-        const credsB64 = btoa(`${ClientID}:${ClientSecret}`)
-        const refreshToken = await getUserData('refreshToken')
+    const responseJSON = await response.json();
 
-        const response = await fetch('https://accounts.spotify.com/api/token', {
-            method: 'POST',
-            headers: {
-                Authorization: `Basic ${credsB64}`,
-                'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            body: `grant_type=refresh_token&refresh_token=${refreshToken}`
-        });
+    if (responseJSON.error) {
+      await this.getTokens();
+    } else {
+      const {
+        access_token: newAccessToken,
+        refresh_token: newRefreshToken,
+        expires_in: expiresIn
+      } = responseJSON;
 
-        const responseJSON = await response.json();
+      const expirationTime = new Date().getTime() + expiresIn * 1000;
 
-        if (responseJSON.error){
-            await this.getTokens()
-        }
-        else {
-            const {
-                access_token: newAccessToken,
-                refresh_token: newRefreshToken,
-                expires_in: expiresIn,
-            } = responseJSON
+      await setUserData('accesssToken', newAccessToken);
 
-            const expirationTime = new Date().getTime() + expiresIn * 1000;
-
-            await setUserData('accesssToken', newAccessToken)
-
-            if (newRefreshToken){
-                await setUserData('refreshToken', newRefreshToken)
-            }
-            await setUserData('expirationTime', expirationTime.toString())
-
-        }
+      if (newRefreshToken) {
+        await setUserData('refreshToken', newRefreshToken);
+      }
+      await setUserData('expirationTime', expirationTime.toString());
     }
-    catch (e){
-        console.log(e)
-    }
-
-
+  } catch (e) {
+    console.log(e);
+  }
 }
 
-async function setUserData(key, value){
-
-    try {
-        await AsyncStorage.setItem(key, value)
-    }
-    catch (e){
-        console.log(e)
-    }
+async function setUserData(key, value) {
+  try {
+    await AsyncStorage.setItem(key, value);
+  } catch (e) {
+    console.log(e);
+  }
 }
 
-async function getUserData(key){
-    try {
-        const value = await AsyncStorage.getItem(key)
-        if (value !== null){
-            return value
-        }
+async function getUserData(key) {
+  try {
+    const value = await AsyncStorage.getItem(key);
+    if (value !== null) {
+      return value;
     }
-    catch (e){
-        console.log(e)
-    }
+  } catch (e) {
+    console.log(e);
+  }
 }
-
 
 //export default {logIn, getTokens, refreshTokens, getUserData}
